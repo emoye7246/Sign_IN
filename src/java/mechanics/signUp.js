e3fff933d51300a38071cd7673f6cd23e5596d0f
Elijah <elijahmoye96@gmail.com>
2024-11-02 02:50:09 -0400

    I love the odin Project
    I wish the odin project can make a unity class
    becuase I would take it but I understand why it
    would be difficult to do so bummer

--- src/java/mechanics/signUp.js
+++ src/java/mechanics/signUp.js
@@ -1,8 +1,11 @@
 export function createSignUp(){
-
+    
+    let inputs = document.querySelectorAll('input')
 
     let loginPage = document.getElementById('login-page')
     loginPage.style.display = 'none'
+
+    let form = document.getElementById('form')
 
 
     let formsContent = document.getElementById('formsContent')
@@ -10,32 +13,71 @@ export function createSignUp(){
 
     let password = document.getElementById('password')
 
+    let createPasswordAsterik = document.createElement('div')
+    createPasswordAsterik.innerHTML = `*`
+    createPasswordAsterik.id = 'asterik'
+
     let createPassword = document.createElement('label')
     createPassword.innerHTML = 'Create Password: '
     createPassword.htmlFor = 'passwordCreate'
- 
+
     let createPasswordInput = document.createElement('input')
     createPasswordInput.id = 'passwordCreate'
+    createPasswordInput.required = true
+
+    let confirmPasswordAsterik = document.createElement('div')
+    confirmPasswordAsterik.innerHTML = '*'
+    confirmPasswordAsterik.id = 'asterik'
  
     let confirmPassword = document.createElement('label')
     confirmPassword.innerHTML = 'Confirm Password: '
     createPassword.htmlFor = 'confirmPassword'
+
  
     let confirmPasswordInput = document.createElement('input')
     confirmPasswordInput.id = 'confirmPassword'
+    confirmPasswordInput.required = true
+
+    let errorMessage = document.createElement('div')
+    
  
-    password.append(createPassword, createPasswordInput, confirmPassword, confirmPasswordInput)
+    password.append(createPasswordAsterik, createPassword, createPasswordInput, confirmPasswordAsterik, confirmPassword, confirmPasswordInput)
 
 
+    
     let sumbit = document.getElementById('sumbit')
 
     sumbit.addEventListener('click', function(event){
 
-        event.preventDefault()
-        console.log('it works')
+        inputs.forEach((element) => {
+
+            if(element.checkValidity() == false){
+
+                element.style.border = '1px solid red'
+            }
+        })
+
+        if(createPasswordInput.value.toString() == confirmPasswordInput.value.toString()){
+
+                console.log('values match')
+
+        }
+
+
+        else if(createPasswordInput.value.toString() != confirmPasswordInput.value.toString()){
+    
+    
+                event.preventDefault()  
+                
+                errorMessage.innerHTML = ' '
+                errorMessage.innerHTML = 'Passwords Do not match'
+                errorMessage.style.color = 'red'
+                
+
+
+                form.insertBefore(errorMessage, sumbit)
+        }
     })
 
-
-
-
 }
+// Passed
