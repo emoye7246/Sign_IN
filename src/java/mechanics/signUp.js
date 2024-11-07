0761869609959fdef9a3b8c3ea8a2fd6a004e62f
Elijah <elijahmoye96@gmail.com>
2024-11-07 01:13:28 -0500

    Excellent day
    Today was a great for the code i love what i just
    accomplished and going to finish this project
    now

--- src/java/mechanics/signUp.js
+++ src/java/mechanics/signUp.js
@@ -8,55 +8,98 @@ export function createSignUp(){
     let formsContent = document.getElementById('formsContent')
     formsContent.style.display = 'flex'
 
-    let createPasswordId = document.getElementById('createPassword')
-
-    let confirmPasswordId = document.getElementById('confirmPassword')
-
-    let createPassword = document.createElement('label')
-    createPassword.innerHTML = 'Create Password: '
-    createPassword.htmlFor = 'passwordCreate'
-
-    let createPasswordInput = document.createElement('input')
-    createPasswordInput.id = 'passwordCreate'
-    createPasswordInput.required = true
- 
-    let confirmPassword = document.createElement('label')
-    confirmPassword.innerHTML = 'Confirm Password: '
-    createPassword.htmlFor = 'confirmPassword'
-
- 
-    let confirmPasswordInput = document.createElement('input')
-    confirmPasswordInput.id = 'confirmPassword'
-    confirmPasswordInput.required = true
-
-    
-    createPasswordId.append(createPassword, createPasswordInput)
-
-    confirmPasswordId.append(confirmPassword, confirmPasswordInput)
-
-
-    formValid(createPasswordInput, confirmPasswordInput)
-
-}
-
-
-function formValid(checkPasswordOne, checkPasswordTwo){
 
     let submit = document.getElementById('submit')
 
+    let inputs = document.querySelectorAll('input')
+
+    let newPassword = document.getElementById('newPassword')
+    let confirmPasswords = document.getElementById('confirmPasswords')
+
+
+    newPassword.addEventListener('keyup', checkPassword)
+
+
     submit.addEventListener('click', function(event){
 
-        if(checkPasswordOne.value.trim() != checkPasswordTwo.value.trim()){
+        inputs.forEach((element) => {
+
+            if(element.checkValidity() == false){
+
+                element.style.border = '1px solid red'
+            }
+        })
+
+        if(newPassword.value.trim() != confirmPasswords.value.trim()){
 
             event.preventDefault()
             console.log('works')
 
         }
-        else if(checkPasswordOne.value == checkPasswordTwo.value){
+        else if(newPassword.value == confirmPasswords.value){
 
             console.log('works')
         }
     })
+
+
+}
+
+function checkPassword(){
+
+    let newPassword = document.getElementById('newPassword')
+
+
+    let special = document.getElementById('special')
+    let characterCount = document.getElementById('characterCount')
+    let searchNumber = document.getElementById('searchNumber')
+    let upperCase = document.getElementById('upperCase')
+
+
+    if(newPassword.value.trim().match(/[^a-zA-Z0-9\s]/g)){
+
+        special.style.color = 'green'
+
+    }
+    else{
+
+        special.style.color = 'lightgray'
+        
+    }
+    // special character
+
+    if(newPassword.value.trim().length >= 4 ){
+
+        characterCount.style.color = 'green'
+    }
+    else{
+
+        characterCount.style.color = 'lightgray'
+    }
+    // character Count
+    
+    if(newPassword.value.trim().match(/[0-9]/)){
+
+        searchNumber.style.color = 'green'
+
+    }
+    else{
+
+        searchNumber.style.color = 'lightgray'
+    }
+    //  Atleast one number
+
+    if(newPassword.value.trim().match(/[A-Z]/)){
+
+        upperCase.style.color = 'green'
+    }
+    else{
+
+        upperCase.style.color = 'lightgray'
+
+    }
+    
+
 
 }
 
